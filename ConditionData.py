4e9442c4387609e3158009c89baf9e3ed2f7c754
TextmarkerOrange <sebastian.wehmeier@gmail.com>
2025-01-05 15:31:10 +0100

    add initvar for  dataset_link

--- ConditionData.py
+++ ConditionData.py
@@ -31,25 +31,37 @@ class ConditionData:
     dataset_name: str
     _conditions: InitVar[list[Callable[[Any], bool]]]
     _rewrite: InitVar[list[Callable[[pd.DataFrame], pd.DataFrame]]] 
-    _delete_colum:InitVar[list[str]]
-    dataset_link: str = ""
+    _delete_colum: InitVar[list[str]]
+    _dataset_link: InitVar[str]
     playerheatmap: player.PlayerHeatMap = field(default_factory=player.PlayerHeatMap)
     flip_sec_half_coordinates: bool = True
 
 
-
-    __delete_colum:list[str]= field( init=False, default_factory=list)
+    
+    __delete_colum: list[str] = field(init=False, default_factory=list)
     __rewrite:list[Callable[[pd.DataFrame], pd.DataFrame]] = field( init=False, default_factory=list)
     __actuell: dict[str, bool] = field(init=False,default_factory=lambda: {"dataset": False, "playerheatmap": False})
     __dataset: pd.DataFrame = field(init=False,default_factory=pd.DataFrame)
     __conditions: list[Callable[[Any], bool]] = field(init=False,default_factory=list)
-
-    def __post_init__(self,_conditions, _rewrite,_delete_colum):
+    __dataset_link: str = field(init=False,default_factory=str)
+    def __post_init__(self, _conditions, _rewrite, _delete_colum, _dataset_link):
         self.__conditions = _conditions
         self.__rewrite = _rewrite
         self.__delete_colum = _delete_colum
+        self.__dataset_link = _dataset_link
+
     @property
-    def conditions(self) ->  list[Callable[[Any], bool]] :
+    def dataset_link(self) -> str:
+        return self.__dataset_link
+
+    @dataset_link.setter
+    def dataset_link(self, new: str):
+        self.__dataset_link = new
+        for i in self.__actuell.keys():
+            self.__actuell[i] = False
+
+    @property
+    def conditions(self) -> list[Callable[[Any], bool]] :
         return self.__conditions
 
     @conditions.setter
@@ -57,11 +69,13 @@ class ConditionData:
         self.__conditions = new_cond
         for i in self.__actuell.keys():
             self.__actuell[i] = False
+
     @property
-    def rewrite(self)-> list[Callable[[pd.DataFrame], pd.DataFrame]]:
+    def rewrite(self) -> list[Callable[[pd.DataFrame], pd.DataFrame]]:
         return self.__rewrite
+
     @rewrite.setter
-    def rewrite(self, new:list[Callable[[pd.DataFrame], pd.DataFrame]]):
+    def rewrite(self, new: list[Callable[[pd.DataFrame], pd.DataFrame]]):
         self.__rewrite = new
         for i in self.__actuell.keys():
             self.__actuell[i] = False
@@ -71,14 +85,17 @@ class ConditionData:
         if not self.__actuell["dataset"]:
             raise ValueError("self.dataset is not up to date")
         return self.__dataset
+
     @property
     def delete_colum(self)-> list[str]:
         return self.__delete_colum
+
     @delete_colum.setter
     def delete_colum(self, new : list[str]):
         self.__delete_colum = new
         for i in self.__actuell.keys():
             self.__actuell[i] = False
+
     def create_conditionData(self, indicator_2half ='matchPeriod' , des_2haf='2H',startx= "pos_orig_x" , starty= "pos_orig_y", endx= "pos_dest_x", endy = "pos_dest_y"):
         """Creats a dataset only with the row who fulfills the conditions"""
         self.__actuell["dataset"]=True
@@ -140,11 +157,11 @@ if __name__ == "__main__":
         print(x["pos_orig_y"])
         return x
     test = ConditionData(dataset_name="Example",_conditions=[lambda x: x["subEventName"] == "Simple pass"],_rewrite=[],_delete_colum = [],
-                        dataset_link="data\\example_data.csv", 
+                        _dataset_link="data\\example_data.csv", 
                         playerheatmap=player.PlayerHeatMap( action_name="Simple pass1",shape=(50,50)))
     test.create_conditionData()
     test2 = ConditionData(dataset_name="Example2",_conditions=[lambda x: x["subEventName"] == "Simple pass"],_rewrite=[testfunc],_delete_colum = ["tags"],
-                        dataset_link="data\\example_data2.csv",playerheatmap=player.PlayerHeatMap( action_name="Simple pass2",shape=(50,50)))
+                        _dataset_link="data\\example_data2.csv",playerheatmap=player.PlayerHeatMap( action_name="Simple pass2",shape=(50,50)))
     test2.create_conditionData()
     test2.creat_file("data\\example_data2")
     plt.figure(1,figsize=(12, 6))
